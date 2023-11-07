f52031ae6ac450b0b93fb308280f4cd3d0f8dcd7
Vinni-3005 <vinithaaduri@gmail.com>
2023-11-07 10:21:34 +0530

    first commit

--- my react app/myapp/basics/src/Message.jsx
+++
@@ -1,8 +0,0 @@
-let Message = ()=>{
-
-    return <div>
-        <h1>HEllo</h1>
-        <h2>Good Morning</h2>
-    </div>
-}
-export default Message
--- my react app/myapp/src/index.js
+++
@@ -1,8 +0,0 @@
-//ReactDOM.render(What you want to render, where you want to render)
-
-import ReactDOM from 'react-dom'
-import App from './App'
-
-
-
-ReactDOM.render(<App/>,document.getElementById('root'))
