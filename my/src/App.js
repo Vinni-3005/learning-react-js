1b0159bb53fad934ab48bdba8fe1e83c998ca802
Vinni-3005 <vinithaaduri@gmail.com>
2023-11-09 10:38:01 +0530

    no commits

--- my/src/App.js
+++ my/src/App.js
@@ -1,28 +1,15 @@
-//react is based on component architecture
-//components & components files tart with capital letters
-//Components - describes the User Interface(UI), peace of reusable code of UI
-//once component is created , we can use n no.of times
-//they are 2 types of components i.e.,
-   // 1. Functional component - fat arrow func md normal func
-   // 2. class component
+import React from 'react'
+import Class1 from './cls - cls/cls1'
 
-
-
-import Header from './components/header'
-import Footer from './components/footer'
-import Service from './components/service'
-
-let App=()=>{         // it is functional component lo fat arrow func
-    return <div>
-        <nav className='navbar navbar-dark bg-dark'>
-            <a href='#' className='navbar-brand'>Functional Component example</a>
-
-        </nav>
-        <Header/>
-        <Footer/>
-        <Service/>
+const App = () => {
+  return (
+    <div>
+        <h1> Class to Func prop example </h1>
+        
+        <Class1 />
+        
     </div>
+  )
 }
+
 export default App
-   
-   
